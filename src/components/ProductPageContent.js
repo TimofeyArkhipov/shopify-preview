517d3d3f67c3c24a0cfad5745a1e766979eaa295
Timofey Arkhipov <oldestspells@gmail.com>
2023-05-03 15:56:50 +0300

    product form UI

--- src/components/ProductPageContent.js
+++ src/components/ProductPageContent.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import Image from 'next/image'
+import ProductForm from './ProductForm'
+
 
 export default function ProductPageContent({ product }) {
   return (
-    <div>
-        {product.title}
+    <div className="flex flex-col justify-center item-center space-y-8 md:flex-row md:items-start md:space-y-0 space-x-4 lg:space-x-8 max-w-6xl w-11/12 mx-auto"> 
+        <div className='w-full max-w-md border bg-white rounded-2xl ovelflow-hidden shadow-lg md:w-1/2'>
+          <div className='relative h-96 w-full'>
+            <Image src={product.images.edges[0].node.originalSrc}
+                    alt={product.images.edges[0].node.altText || ""}
+                    fill
+                    style='cover'
+            />
+          </div>
+        </div>
+        <ProductForm product={product}/>
     </div>
   )
 }
--- src/pages/products/[product].js
+++ src/pages/products/[product].js
@@ -5,7 +5,7 @@ import React from 'react';
 
 export default function ProductPage({ product }) {
   return (
-    <div>
+    <div className="min-h-screen py-12 sm:pt-20"> 
         <ProductPageContent product={product}/>
     </div>
   )
